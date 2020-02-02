9bcb78053c7b4a635b2e81c4aae6d56e086ee90e
Timur <timur.rasulev.82@gmail.com>
2020-02-02 23:37:29 +0300

    check invariants

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { distanceArray, distanceGeneric } from "./composition/tuples";
 import { distanceRecord } from "./composition/record";
+import { checkInvariants } from "./composition/invariants";
 
 document.getElementById("app").innerHTML = `
 <h1>Hello Types!</h1>
@@ -10,3 +11,4 @@ document.getElementById("app").innerHTML = `
 
 console.log("tuples", distanceArray([1, 0], [5, 2]));
 console.log("records", distanceRecord({ x: 1, y: 2 }, { x: 2, y: 5 }));
+console.log("check invariants", checkInvariants(5, 20));
